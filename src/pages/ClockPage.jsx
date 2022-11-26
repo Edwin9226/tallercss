1f17efcb24587be6c82b668210b78e66fc6f0587
Edwin <miguepinchao1992@gmail.com>
2022-11-25 20:35:42 -0600

    Clock

--- src/pages/ClockPage.jsx
+++ src/pages/ClockPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Clock from '../components/Clock'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import "../styles/home.css"
 const ClockPage = () => {
   return (
-    <div>ClockPage</div>
+    <div>
+      <Navbar/>
+      <Clock/>
+      <Footer/>
+    </div>
   )
 }
 
